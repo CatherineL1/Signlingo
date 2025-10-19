d5dc861d39866441f202b416913cf49744a05172
CatherineL1 <catherine.lin@vhhscougars.org>
2025-10-19 00:07:47 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,9 @@
-
+const CACHE = 'sl-v1';
+const FILES = [
+  '/',
+  '/index.html',
+  '/model/model.json',
+  '/model/weights.bin'
+];
+self.addEventListener('install', e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
+self.addEventListener('fetch', e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
